cd0cc8cfb589f66fc39eb4f2f918eca454af8905
Paweł Palacz <ppalacz@edu.cdv.pl>
2021-04-07 12:18:36 +0200

    backend done

---
+++ src/Entity/Customer.php
@@ -0,0 +1,163 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\CustomerRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CustomerRepository::class)
+ *
+ * @ApiResource()
+ */
+class Customer
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $firstName;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $lastName;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $street;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $number;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $flat;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $phoneNo;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $ordersNo;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): self
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): self
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    public function getStreet(): ?string
+    {
+        return $this->street;
+    }
+
+    public function setStreet(string $street): self
+    {
+        $this->street = $street;
+
+        return $this;
+    }
+
+    public function getNumber(): ?string
+    {
+        return $this->number;
+    }
+
+    public function setNumber(string $number): self
+    {
+        $this->number = $number;
+
+        return $this;
+    }
+
+    public function getFlat(): ?string
+    {
+        return $this->flat;
+    }
+
+    public function setFlat(?string $flat): self
+    {
+        $this->flat = $flat;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getPhoneNo(): ?string
+    {
+        return $this->phoneNo;
+    }
+
+    public function setPhoneNo(string $phoneNo): self
+    {
+        $this->phoneNo = $phoneNo;
+
+        return $this;
+    }
+
+    public function getOrdersNo(): ?int
+    {
+        return $this->ordersNo;
+    }
+
+    public function setOrdersNo(int $ordersNo): self
+    {
+        $this->ordersNo = $ordersNo;
+
+        return $this;
+    }
+}
